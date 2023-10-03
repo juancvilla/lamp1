a1d9292afe6c8369a60bdae64bd690b9cf2b85f7
Juan Carlos Villarroel Alvarez <juancvilla@gmail.com>
2023-10-03 15:18:07 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello from php! <BR>";
+
+$dbhost = '127.0.0.1'
+$dbuser = 'user'
+$dbpass = 'pass'
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
+
+if ($conn->connect_error) {
+  // print_r($conn);
+  die("COnnection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$database = mysqli_select_db($conn, 'db');
+
+?>
