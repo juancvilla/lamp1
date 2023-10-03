60d00e8111304a391daf717d29ee0d9c540d54fb
Juan Carlos Villarroel Alvarez <juancvilla@gmail.com>
2023-10-03 16:55:05 -0300

    Update index.php
    correction: crbug/1173575

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
-echo "Hello from php! <BR>";
-
-$dbhost = '127.0.0.1'
-$dbuser = 'user'
-$dbpass = 'pass'
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass);
-
-if ($conn->connect_error) {
-  // print_r($conn);
-  die("COnnection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$database = mysqli_select_db($conn, 'db');
-
+        echo "Hello from php! <BR>";
+        $dbhost = '127.0.0.1';
+        $dbuser = 'user';
+        $dbpass = 'pass';
+        $conn = new mysqli($dbhost, $dbuser, $dbpass);
+        if ($conn->connect_error) {
+                // print_r($conn);
+                die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+        $database = mysqli_select_db($conn, 'db');
 ?>
