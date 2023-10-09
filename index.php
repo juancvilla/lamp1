5bbe178ad956261d519d25f932418ea6e2127b2b
Juan Carlos Villarroel Alvarez <juancvilla@gmail.com>
2023-10-09 18:39:03 -0300

    Update index.php
    Display Date and Time

--- index.php
+++ index.php
@@ -11,3 +11,16 @@
         echo "Connected successfully";
         $database = mysqli_select_db($conn, 'db');
 ?>
+<html> 
+	<head> 
+	<title>PHP Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP Test</h1>
+		<p><b>An juancvilla Example of PHP in Action</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	</body> 
+</html>
